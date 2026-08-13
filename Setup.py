9d03e6fe59a2f7f809fef8a6b7ace7f2d6a2163f
EHESPO International nonprofit corporation <executive@eheps.org>
2026-08-13 15:38:23 -0400

    Add setup instructions to Setup.py
    Added setup instructions for cloning, installing dependencies, running tests, and linting.

---
+++ Setup.py
@@ -0,0 +1,12 @@
+# Clone repository
+git clone https://github.com/EHEPS-International/eheps-pki.git
+cd eheps-pki
+
+# Install dependencies
+make install
+
+# Run tests
+make test
+
+# Lint configurations
+make lint
